66470efa6447e8f809406688048bc50097d74845
pp9191 <pp9191@qq.com>
2019-06-19 22:32:47 +0800

    1

--- src/main/java/com/web/config/MyWebConfig.java
+++ src/main/java/com/web/config/MyWebConfig.java
@@ -1,9 +1,7 @@
 package com.web.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.web.interceptor.LayoutInterceptor;
@@ -11,12 +9,6 @@ import com.web.interceptor.LayoutInterceptor;
 @Configuration
 public class MyWebConfig implements WebMvcConfigurer {	
 	
-	/*
-	 * @Override public void addViewControllers(ViewControllerRegistry registry) {
-	 * registry.addViewController("/").setViewName("index");
-	 * registry.setOrder(Ordered.HIGHEST_PRECEDENCE); }
-	 */
-
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new LayoutInterceptor()).addPathPatterns("/**");
